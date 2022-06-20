06707955809828b4ede32854c2605668c69b7ca3
Rio Tolinggi <lovelytolinggi@gmail.com>
2022-06-20 13:39:42 +0800

    push github Authorization

--- app/routes/index.tsx
+++
@@ -1,8 +0,0 @@
-import { getUser } from "~/utils/auth.server";
-import type { LoaderFunction } from "@remix-run/node";
-import { redirect } from "@remix-run/node";
-
-export const loader: LoaderFunction = async ({ request }) => {
-  const isAuth = await getUser(request);
-  return isAuth ? redirect("/movie") : redirect("/auth");
-};
